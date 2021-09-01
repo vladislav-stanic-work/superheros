5c065a71725169528807ae8895286c08a772141e
vladislav-stanic-work <vladislav.stanic.work@gmail.com>
2021-09-01 13:54:35 +0200

    Material added with button

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'superheros-root',
+  selector: 'sup-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- apps/main/src/app/app.module.ts
+++ apps/main/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { UiLibraryModule } from '@superheros/ui-library';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, UiLibraryModule],
   providers: [],
   bootstrap: [AppComponent],
 })
